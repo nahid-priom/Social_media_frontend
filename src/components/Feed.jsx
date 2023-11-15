db0bcf76f4df5495265208e16b3ecf9770d09d0a
nahid-priom <nahid.priom.06@gmail.com>
2023-11-15 23:15:24 +0600

    Spinnder add and category filter in homepage

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+import { client } from '../client';
+import { feedQuery, searchQuery } from '../utils/data';
+
+import Spinner from './Spinner';
+import MasonryLayout from './MasonaryLayout';
 
 const Feed = () => {
+  const [pins, setPins] = useState();
+  const [loading, setLoading] = useState(false);
+  const { categoryId } = useParams();
+
+  useEffect(() => {
+    if (categoryId) {
+      setLoading(true);
+      const query = searchQuery(categoryId);
+      client.fetch(query).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    } else {
+      setLoading(true);
+
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    }
+  }, [categoryId]);
+  const ideaName = categoryId || 'new';
+  if (loading) {
+    return (
+      <Spinner message={`We are adding ${ideaName} ideas to your feed!`} />
+    );
+  }
   return (
     <div>
-      Feed
+      {pins && (
+        <MasonryLayout pins={pins} />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Feed
+export default Feed;
--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,8 @@
-export {default as UserProfile} from './UserProfile'
-export {default as Login} from './Login'
-export {default as Feed} from './Feed'
-
-export {default as Search} from './Search'
-
-export {default as PinDetail} from './PinDetail'
-
-export {default as CreatePin} from './CreatePin'
-
-export {default as Navbar} from './Navbar'
-
+export { default as Navbar } from './Navbar';
+export { default as Feed } from './Feed';
+export { default as PinDetail } from './PinDetail';
+export { default as UserProfile } from './UserProfile';
+export { default as CreatePin } from './CreatePin';
+export { default as Search } from './Search';
+export { default as Login } from './Login';
+export { default as MasonryLayout } from './MasonaryLayout';
